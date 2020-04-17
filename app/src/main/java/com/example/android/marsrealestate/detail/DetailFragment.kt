54740429685cda59a96884113a9bf3a509bc3e15
Idris Adenopo <iadenop2@jaguarlandrover.com>
2020-04-17 14:56:10 +0100

    complete lesson 8

--- app/src/main/java/com/example/android/marsrealestate/detail/DetailFragment.kt
+++ app/src/main/java/com/example/android/marsrealestate/detail/DetailFragment.kt
@@ -21,6 +21,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.example.android.marsrealestate.databinding.FragmentDetailBinding
 
@@ -34,7 +35,14 @@ class DetailFragment : Fragment() {
         @Suppress("UNUSED_VARIABLE")
         val application = requireNotNull(activity).application
         val binding = FragmentDetailBinding.inflate(inflater)
-        binding.setLifecycleOwner(this)
+        binding.lifecycleOwner = this
+
+        val marsProperty = DetailFragmentArgs.fromBundle(arguments!!).selectedProperty
+
+        val viewModelFactory = DetailViewModelFactory(marsProperty, application)
+
+        binding.viewModel = ViewModelProvider(
+                this, viewModelFactory).get(DetailViewModel::class.java)
         return binding.root
     }
 }
